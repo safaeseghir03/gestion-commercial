868aa1ed78179d0c54814ef03a0c9468a5e077ac
safae <safaerryihm@gmail.com>
2021-11-01 00:58:33 +0100

    commit 2

---
+++ connexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title></title>
+	    <link rel="stylesheet" href="creeruncompt.css">
+	<body>
+		<form method="POST">
+			<h2>Formule d'inscription</h2>
+			<label>User Name :</label>
+			<input type="text" name="username" placeholder="User Name"><br>
+			<label>Password:</label>
+			<input type="text" name="password" placeholder="Password"><br>
+			<button type="submit" name="submit">Login</button>
+		</form>
+<?php
+
+?>
+</body>
+</head>
+</html>
