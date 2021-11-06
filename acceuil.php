580c3f37f18a5776623417829e57e75b532f0547
safae <safaerryihm@gmail.com>
2021-11-06 20:33:58 +0100

    commit 3

---
+++ acceuil.php
@@ -0,0 +1,48 @@
+
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+
+  <link rel="stylesheet" type="text/css" href="css/Bootstrap.css">
+   <link rel="stylesheet" type="text/css" href="css/zak.css">
+
+
+
+
+</head>
+
+
+<body>
+<div  style=" margin:20px;"><b  style="font-size:33px;color: #191970; font: bold;  ">Bonjour et bienvenue, admin sur mon site gestion commerciale</b></div>
+<div align="RIGHT" ><span style="border:1.25px solid black; border-radius:33PX; background-color:#318CE7; "><img src="images_home/deconnx.png" style="height:15px;  width:18px; margin-right:2px;" /><a href="index.php" style="color:  #fff; font-size:15.5px; text-decoration:none; font-family:arial "><b>DECONNEXION </b></a></span></div>
+<div Align="center" style="padding-top:28px; font-size:31px;color: #000000; font: bold; "><b>
+Ici vous trouverez toutes vos pages </b></div>
+
+ <div class="home" align="center">
+<div class="container client" align="center"><a href="clients.php"><img src="images_home/clients.png" alt="clients" title="clients" class="ok"/><div align="center" class="caption"><marquee behavior="alternate" DIRECTION="right">Clients</marquee></div></a></div>
+<div class="container produit" align="center"> <a href="produits.php"><img src="images_home/produits.png" alt="produits" title="produits" class="ok"/><div align="center" class="caption"><marquee behavior="alternate" DIRECTION="right" >Produits</marquee></div></a></div>
+   <div class="container statistiques" align="center"><a href="statistiques.php"><img src="images_home/statistiques.gif" alt="statistiques" title="statistiques" class="ok" /><div align="center" class="caption"><marquee behavior="alternate" DIRECTION="right" >Statistiques</marquee></div></a></div>
+    <div class="container detailscmd" align="center"> <a href="details_commande.php"><img src="images_home/det.svg" alt="detailscommandes" title="details commandes" class="ok"/><div align="center" class="caption"><marquee behavior="alternate" DIRECTION="right" >Details commandes</marquee></div></a></div>
+    <div class="container cmd" align="center"><a href="commandes.php"><img src="images_home/commandes.png" alt="commandes" title="commandes" class="ok"/><div align="center" class="caption"><marquee behavior="alternate" DIRECTION="right" > commandes</marquee></div></a></div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+</html>
